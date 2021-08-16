2b8c67d36fa074a1c2aa6c9be5c150feb3d1f844
artdev-hash <kim.dev.pr@gmail.com>
2021-08-16 12:03:55 +0800

    initial

--- kshared-nocto-copier.cmd
+++ kshared-nocto-copier.cmd
@@ -1,2 +1,2 @@
-net use f:\\192.168.254.01\MY File {123456} /user:Ubuntu\admin-01 /p:yes
+net use f:\\192.168.254.01\MY File {123456} /user:Ubuntu\admin-01 /P:yes
 xcopy C:\test "192.168.254.01\MY File" /w /f /j /s /z
